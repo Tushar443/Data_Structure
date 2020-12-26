15e29da17881dcd1aa2f98ec507f94429139dfb9
Tushar443 <65149782+Tushar443@users.noreply.github.com>
2020-12-26 19:57:59 +0530

    Doubly Link List

--- Exam_Practice/DoublyLinkList.cpp
+++ Exam_Practice/DoublyLinkList.cpp
@@ -62,6 +62,54 @@ class DoublyLink{
             }
         }
 
+        void InsertAfterData(int data, int after){
+            Node* node = new Node(data);
+            if(head == NULL){
+                head= node;
+            }
+            else if(head->getData()==after){
+                head->getNext()->setPrev(node);
+               
+                node->setNext(head->getNext());
+                node->setPrev(head);
+                head->setNext(node);
+            }else{
+                Node* temp = head;
+                while(temp->getData()!=after){
+                    temp=temp->getNext();
+                }
+                temp->getNext()->setPrev(node);
+                node->setNext(temp->getNext());
+                temp->setNext(node);
+                node->setPrev(temp);
+
+            }
+        }
+        
+        void InsertBeforeData(int data, int before){
+            Node* node = new Node(data);
+            if(head==NULL){
+                head=node;
+            }
+            else if(head->getData()==before)
+            {
+                head->setPrev(node);
+                node->setNext(head);
+                head= node;
+            }else{
+                Node* temp=head;
+                while (temp->getData()!=before){
+                    temp=temp->getNext();
+                }
+                
+                node->setPrev(temp->getPrev());
+                temp->setPrev(node);
+                node->setNext(temp);
+                node->getPrev()->setNext(node); 
+            }
+            
+        }
+
         void Display(){
             Node* temp = head;
             while (temp->getNext()!=NULL)
@@ -84,17 +132,22 @@ class DoublyLink{
 int main(){
 
     DoublyLink d ;
-    //  d.Insert(10);
-    //  d.Insert(20);
-    //  d.Insert(30);
-    //  d.Insert(40);
-    //  d.Insert(50);
-    //  d.Display();
-     d.InsertBefore(10);
-     d.InsertBefore(20);
-     d.InsertBefore(30);
-     d.InsertBefore(40);
-     d.InsertBefore(50);
+     d.Insert(10);
+     d.Insert(20);
+     d.Insert(30);
+     d.Insert(40);
+     d.Insert(50);
+     //d.InsertAfterData(25,20);
+     //d.InsertAfterData(15,10);
+     d.InsertBeforeData(15,20);
+     d.InsertBeforeData(5,10);
      d.Display();
+    //  d.InsertBefore(10);
+    //  d.InsertBefore(20);
+     
+    //  d.InsertBefore(30);
+    //  d.InsertBefore(40);
+    //  d.InsertBefore(50);
+     //d.Display();
     return 0;
 }
